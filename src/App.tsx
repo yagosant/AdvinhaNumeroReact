0db36fc38c01e6f01360aba933eb10a396c42eef
Yago <yagojf27@gmail.com>
2022-10-17 20:27:53 -0300

    fix: start

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,30 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const [numero1, setNumero1] = useState("");//use state retorna um array
+
+  let numSurpresa = Math.floor(Math.random() * 10);
+
+  function validarNumero() {
+    if (parseInt(numero1) == numSurpresa)alert("caraca eles são iguais");
+    alert(`O número surpresa era ${numSurpresa} e você informou ${numero1}`);
+    
+  }
+
+ useEffect(()=>{
+  if((parseInt(numero1) + 3) == numSurpresa) alert(`Quaaaaase o número surpresa era ${numSurpresa} e você informou ${numero1}`)
+ },[numSurpresa]);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Advinha os números Aeee</h2>
+     <form action="">
+     <label htmlFor="">Chuta um número Ai: </label>
+     <input type="number" name="numero1" id={numero1} value={numero1} onChange={(e) => setNumero1(e.target.value)} placeholder='informe o número...'/>
+     </form>
+     <button onClick = {validarNumero}>Chutar</button>
     </div>
   );
 }
